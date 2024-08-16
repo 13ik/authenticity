1fed91bc2add10701b8f5f6bbc227e03203ed644
ik01k <168076930+ik01k@users.noreply.github.com>
2024-08-16 22:10:57 +0200

    Added checking validation clg code

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,7 @@ from aiogram.types import Message
 from src.config import BOT_TOKEN
 from src.web_interaction import interact_with_webpage
 
-bot = Bot(token="your_token_bot")
+bot = Bot(token="6526749723:AAEDtiEKNgwZj4yyFcfUoF8MRbo01OGYQTY")
 dp = Dispatcher()
 
 @dp.message(Command("start"))
@@ -15,13 +15,16 @@ async def start(message: Message):
 @dp.message()
 async def handle_code(message: Message):
     auth_code = message.text 
-
-    result = await interact_with_webpage(auth_code)
-    
+    await message.reply("Checking your clg code, wait a couple of seconds please...")
+    if not auth_code.isdigit() or len(auth_code) != 12:
+        await message.answer("Please enter the code again. It's a wrong code.")
+        return
+    elif len(auth_code) == 12: 
+        result = await interact_with_webpage(auth_code)
     await message.reply(result)
 
 async def on_startup(dp: Dispatcher):
-    print("Bot has started")
+    pass
 
 async def main():
     await bot.delete_webhook()
